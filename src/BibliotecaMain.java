e677f7d2c83d3e1981fb636f04fb27dce7c662e1
rafaelcleversystems <rafaelmr@gmail.com>
2024-11-07 22:40:44 -0300

    Criar variável de controle de versão

--- src/BibliotecaMain.java
+++ src/BibliotecaMain.java
@@ -4,12 +4,14 @@ import view.TelaLogin;
 
 public class BibliotecaMain {
 
+	public static String version = "1.0";
+
 	public static void main(String[] args) {
 		System.out.println("Iniciar Sistema.");
 		SwingUtilities.invokeLater(() -> {
-            TelaLogin login = new TelaLogin();
-            login.setVisible(true);
-        });
+			TelaLogin login = new TelaLogin();
+			login.setVisible(true);
+		});
 	}
 
 }
