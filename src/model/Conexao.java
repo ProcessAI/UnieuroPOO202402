b20d7ac911e4443dd48c3f80d4a3c49f16de8301
Ageubr <ageu3561@gmail.com>
2024-11-29 09:25:31 -0300

    Update Conexao.java

--- src/model/Conexao.java
+++ src/model/Conexao.java
@@ -12,28 +12,36 @@ public class Conexao {
     
     //sigleton
     private static Connection conexao = null;
-    
 
+    // Método para testar a conexão
     public static void testConnection() {
-    	if(Conexao.conexao == null)
-    		System.out.println("BD NÃO CONECTADO");
-    	else
-    		System.out.println("BD CONECTADO");
-    }	
+        if (Conexao.conexao == null)
+            System.out.println("BD NÃO CONECTADO");
+        else
+            System.out.println("BD CONECTADO");
+    }
 
-	public static Connection getConexao() {
-		try {
-			
-			if(Conexao.conexao == null)
-			{
-				Conexao.conexao = DriverManager.getConnection(URL, USER, PASSWORD);
-			}
-			return Conexao.conexao;
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-    
+    // Método para obter a conexão
+    public static Connection getConexao() {
+        try {
+            if (Conexao.conexao == null || Conexao.conexao.isClosed()) {  // Verifica se a conexão está fechada
+                Conexao.conexao = DriverManager.getConnection(URL, USER, PASSWORD);
+            }
+            return Conexao.conexao;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // Método para fechar a conexão
+    public static void fecharConexao() {
+        try {
+            if (conexao != null && !conexao.isClosed()) {
+                conexao.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
