a75114bf67514f066bb81b4aaa648d4293c4e6d3
Thalita <thalitasouzaa009@gmail.com>
2024-11-29 09:41:19 -0300

    Add files via upload

---
+++ src/controller/EditoraController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import dao.EditoraDAO;
+import model.Editora;
+import view.EditoraView; 
+
+import java.util.List;
+
+public class EditoraController {
+
+    private EditoraDAO editoraDAO;
+    private EditoraView view; 
+
+    public EditoraController(EditoraView view) {
+        editoraDAO = new EditoraDAO();
+        this.view = view;  
+    }
+
+    public void adicionarEditora(String nome) {
+        Editora editora = new Editora(nome);
+        editoraDAO.addEditora(editora);
+        atualizarTabelaNaView();  
+    }
+
+    public List<Editora> listarEditoras() {
+        return editoraDAO.listarEditoras();
+    }
+
+    public void atualizarEditora(int id, String novoNome) {
+        Editora editora = new Editora(id, novoNome);
+        editoraDAO.updateEditora(editora);
+        atualizarTabelaNaView();  
+    }
+
+    public void deletarEditora(int id) {
+        editoraDAO.deleteEditora(id);
+        atualizarTabelaNaView();  
+    }
+
+    public void atualizarTabelaNaView() {
+        List<Editora> editoras = listarEditoras();  
+        view.atualizarTabela(editoras);  
+    }
+}
